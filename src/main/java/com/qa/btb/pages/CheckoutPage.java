1281d50f321ec67a48198c34356f590478640710
unknown <biradarmohan0@gmail.com>
2025-01-13 16:47:46 +0530

    added locators

--- src/main/java/com/qa/btb/pages/CheckoutPage.java
+++ src/main/java/com/qa/btb/pages/CheckoutPage.java
@@ -1,7 +1,11 @@
 package com.qa.btb.pages;
 
-public class CheckoutPage {
+import org.openqa.selenium.By;
 
+public class CheckoutPage{
 	int a=10;
-	
+	private By name= By.id("test");
+	public void getname() {
+		System.out.println("hello");
+	}
 }
